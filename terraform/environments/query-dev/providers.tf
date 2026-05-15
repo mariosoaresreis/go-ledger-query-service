3c7d42b258f7d030f10ca2a2715277d8b117fc2b
Mario Reis <mario_reis_jr@hotmail.com>
2026-05-15 11:36:27 -0300

    Fix query Terraform providers to bootstrap new GKE cluster

--- terraform/environments/query-dev/providers.tf
+++ terraform/environments/query-dev/providers.tf
@@ -2,26 +2,16 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
-
 data "google_client_config" "default" {}
-
-data "google_container_cluster" "query_gke" {
-  name     = "${var.project_id}-${var.environment}-query-gke"
-  location = "${var.region}-b"
-  project  = var.project_id
-}
-
 provider "kubernetes" {
-  host                   = "https://${data.google_container_cluster.query_gke.endpoint}"
+  host                   = "https://${module.gke.cluster_endpoint}"
   token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(data.google_container_cluster.query_gke.master_auth[0].cluster_ca_certificate)
+  cluster_ca_certificate = base64decode(module.gke.cluster_ca_certificate)
 }
-
 provider "helm" {
   kubernetes {
-    host                   = "https://${data.google_container_cluster.query_gke.endpoint}"
+    host                   = "https://${module.gke.cluster_endpoint}"
     token                  = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(data.google_container_cluster.query_gke.master_auth[0].cluster_ca_certificate)
+    cluster_ca_certificate = base64decode(module.gke.cluster_ca_certificate)
   }
 }
-
